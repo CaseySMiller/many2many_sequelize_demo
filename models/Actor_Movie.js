09284fd9e7c8a50b5df3ea406c5fda760d72266a
CaseySMiller <CaseySMiller@yahoo.com>
2024-04-17 12:58:52 -0600

    added data to through table and updated seeds

--- models/Actor_Movie.js
+++ models/Actor_Movie.js
@@ -1,6 +1,6 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Actor_Movie extends Model {}
 
@@ -14,13 +14,26 @@ Actor_Movie.init(
       autoIncrement: true,
       unique: true,
     },
+    // when we define our through table, we can add any extra information to it that we want.
+    character_name: {
+      type: DataTypes.STRING,
+    },
+    performance_rating: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      //check to make sure the rating is between 1 and 5
+      validate: {
+        isIn: {
+          args: [[1, 2, 3, 4, 5]],
+          msg: "Rating must be an integer between 1 and 5",
+        },
+      }
+    },
+    // sequelize will automatically build actor_id and movie_id columns
   },
-
-  // sequelize will automatically build actor_id and movie_id columns
-
   {
     sequelize,
-    modelName: 'actor_movie', //sequelize will make this table as actor_movies
+    modelName: "actor_movie", //sequelize will make this table as actor_movies
     // freezeTableName: true,
     timestamps: false,
     underscored: true,
--- seeds/actor-seeds.js
+++ seeds/actor-seeds.js
@@ -1,22 +1,25 @@
-const { Actor, Movie, Actor_Movie  } = require('../models');
+const { Actor, Movie, Actor_Movie } = require("../models");
 
 // define actor data as an array of objects
 const actorData = [
   {
-    name: 'Robert Downey Jr.',
-    birth_date: '1965-04-04',
-    movies: [1,2]
+    name: "Robert Downey Jr.",
+    birth_date: "1965-04-04",
+    movies: [
+      { id: 1, performance_rating: 5, character_name: "Tony Stark" },
+      { id: 2, performance_rating: 4, character_name: "Kirk Lazarus" },
+    ],
   },
   {
-    name: 'Chris Evans',
-    birth_date: '1981-06-13',
-    movies: [1,2]
-  }, 
+    name: "Chris Evans",
+    birth_date: "1981-06-13",
+    movies: [{ id: 1 }, { id: 2 }],
+  },
   {
-    name: 'Dough Boy',
-    birth_date: '1981-06-13',
-    movies: [1,3]
-  }, 
+    name: "Dough Boy",
+    birth_date: "1981-06-13",
+    movies: [{ id: 1 }, { id: 3 }],
+  },
 ];
 
 //seed actors and set up associations
@@ -26,36 +29,50 @@ const seedActors = async () => {
     const newActor = await Actor.create(actor);
     // if there's movie data, loop over it and create the association in actor_movie table
     if (actor.movies) {
-      for (const movieId of actor.movies) {
-        // create a new association in actor_movie table for each id in the movies array
-        const newAssociation = await Actor_Movie.create({
+      for (const movie of actor.movies) {
+        // build the association data
+        const newAssociation = {
           actorId: newActor.id, // this column is named actor_id in the table
-          movieId: movieId, // this column is named movie_id in the table
-        });
-        // console.log(newAssociation.get({ plain: true });
+          movieId: movie.id, // this column is named movie_id in the table
+        };
+        // if there's additional data for the association, add it to the object
+        if (movie.performance_rating) {
+          newAssociation.performance_rating = movie.performance_rating
+        }
+        if (movie.character_name) {
+          newAssociation.character_name = movie.character_name
+        }
+        // create the new association in actor_movie table for each id in the movies array
+        const newActorMovie = await Actor_Movie.create(newAssociation);
+        console.log( newActorMovie.get({ plain: true }) );
       }
     }
   }
-}
+};
 
 //seed an actor and a movie while setting up the relationship at the same time
 const seedActorAndMovie = async () => {
   await Actor.create(
     {
-      name: 'Bob Jenkins',
-      birth_date: '1980-02-29',
+      name: "Bob Jenkins",
+      birth_date: "1980-02-29",
+      // we can add a movie and automatically associate it like this
       movies: [
         {
           title: "Seed This!",
-          release_date: '2012-05-04',
+          release_date: "2012-05-04",
+          // we can add info to the through table like this
+          actor_movie: {
+            character_name: "Bob",
+            performance_rating: 5,
+          },
         },
-      ]
+      ],
     },
     {
       include: Movie,
     }
   );
-}
+};
 
-module.exports = {seedActorAndMovie, seedActors};  
-
+module.exports = { seedActorAndMovie, seedActors };
